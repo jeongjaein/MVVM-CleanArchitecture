9fa499fa810e96cd8d3e0b266fd75f814cf415af
jeongjaein <wodls96@gmail.com>
2021-04-07 23:42:33 +0900

    init MVVM + CleanArchitecture

---
+++ MVVM+CleanDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM+CleanDemo
+//
+//  Created by 정재인 on 2021/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
